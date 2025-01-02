fabe57f85331d7bdbf13bfa22341fbb12836ed59
ArielBitan <joekjoek33@gmail.com>
2025-01-02 15:17:13 +0200

    גדגד

--- client/src/components/HomePage/RestaurantCard.tsx
+++ client/src/components/HomePage/RestaurantCard.tsx
@@ -1,11 +1,41 @@
 import { IRestaurant } from "@/types/restaurantTypes";
+import { FaStar } from "react-icons/fa6";
 
 interface RestaurantCardProps {
   item: IRestaurant;
 }
 
 const RestaurantCard: React.FC<RestaurantCardProps> = ({ item }) => {
-  return <div className="w-[270px]">{item.name}</div>;
+  return (
+    <div className="w-[320px] border mb-4 shadow-lg">
+      <div>
+        <img src={item.background_image} alt="background_image" />
+        <div>
+          <div className="px-4 pt-4">
+            <div className="mb-2">
+              <h3 className="font-bold text-lg">{item.name}</h3>
+              <p>{item.cuisine_types.join(", ")}</p>
+            </div>
+            <div>
+              <div className="flex gap-1 items-center">
+                <div className="flex items-center gap-1">
+                  <FaStar className="text-yellow-500 mb-1" />
+                  <span>0</span>
+                </div>
+                <span>•</span>
+                <span>{`משלוח ₪${item.delivery_fee || 0}`}</span>
+                <span>•</span>
+                <span>{`${item.delivery_time || 0} דק'`}</span>
+              </div>
+              <div className="mb-4">
+                <span>{`מינימום הזמנה  ₪${item.min_order || 0}`}</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default RestaurantCard;
