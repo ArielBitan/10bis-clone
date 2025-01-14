72ca1b7e6077720f54df18036bea343182dc3a19
ArielBitan <joekjoek33@gmail.com>
2025-01-14 10:02:34 +0200

    Remove unused navigation button from FirstContent component

--- client/src/components/LandingPage/FirstContent.tsx
+++ client/src/components/LandingPage/FirstContent.tsx
@@ -1,12 +1,10 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
 import PlacesAutocomplete from "./PlacesAutocomplete";
 
 const FirstContent = () => {
   const [selectedOption, setSelectedOption] = useState<"delivery" | "pickup">(
     "delivery"
   );
-  const navigate = useNavigate();
   return (
     <div className="flex flex-col items-center justify-center w-[90%] mx-auto bg-black/60 rounded-lg py-16 relative max-w-[1170px]">
       <div>
@@ -45,15 +43,6 @@ const FirstContent = () => {
 
         <div className="relative flex justify-center w-full mb-6">
           <PlacesAutocomplete />
-        </div>
-
-        <div className="flex justify-center">
-          <button
-            className="h-12 text-lg font-bold text-white bg-blueButton w-full max-w-[90%] hover:bg-blue-600"
-            onClick={() => navigate("/home")}
-          >
-            לכל המסעדות והמבצעים
-          </button>
         </div>
       </div>
 
